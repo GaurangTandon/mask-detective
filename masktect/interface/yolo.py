41f48b1b7ef18acc8c888f664bb0a81136d44b61
Gaurang Tandon <1gaurangtandon@gmail.com>
2021-10-24 02:22:31 +0530

    Added iou, augmentations, trajectory

--- masktect/interface/yolo.py
+++ masktect/interface/yolo.py
@@ -1,5 +1,6 @@
 import collections
 import dataclasses
+from math import sqrt
 
 import os
 import typing
@@ -10,6 +11,19 @@ import tqdm
 
 from .video import ImageSequence
 from ..classifier.train import load_model
+
+VID_WIDTH = 640
+
+VID_HEIGHT = 480
+
+
+def rel_to_abs(x, y):
+    return x * VID_WIDTH, y * VID_HEIGHT
+
+
+def take_dist(x1, y1, x2, y2):
+    (x1, y1), (x2, y2) = rel_to_abs(x1, y1), rel_to_abs(x2, y2)
+    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
 @dataclasses.dataclass
@@ -38,6 +52,9 @@ class Box:
 
         return (x1, y1), (x2, y2)
 
+    def get_centroid(self):
+        return self.x, self.y
+
 
 class VideoAnnotator:
 
@@ -54,7 +71,7 @@ class VideoAnnotator:
             --hide-labels --hide-conf --save-txt
         """)
 
-    def annotations(self, root_path: str = "weights/yolo/runs/detect/exp/labels/"):
+    def annotations(self, root_path: str = "weights/yolo/runs/detect/exp9/labels/"):
         """
         Gets the annotations in a usable format
         :param root_path: The prefix of all annotation files
@@ -66,8 +83,8 @@ class VideoAnnotator:
             bounding_boxes = []
             with open(frame_path) as f:
                 for line in f:
-                    c, x, y, w, h = map(float, line.split(' '))
-                    bounding_boxes.append(Box(label=c, x=x, y=y, w=w, h=h, group=0))
+                    _, x, y, w, h = map(float, line.split(' '))
+                    bounding_boxes.append(Box(label=0, x=x, y=y, w=w, h=h, group=0))
             return bounding_boxes
 
         frames_with_bounding_boxes = []
@@ -113,4 +130,73 @@ class VideoAnnotator:
             for (index, label) in zip(batch_indices, classification):
                 annotation_data[index[0]][index[1]].label = label
 
-        return annotation_data
+        self.annotation_data = annotation_data
+        return self.annotation_data
+
+    def apply_iou(self):
+        THRESH = 1  # decide good threshold
+
+        group_number = 1
+
+        for frame_idx in range(0, len(self.annotation_data) - 1):
+            this_frame = self.annotation_data[frame_idx]
+            next_frame = self.annotation_data[frame_idx + 1]
+            used = [False for _ in range(len(next_frame))]
+
+            for box in this_frame:
+                if box.group == 0:
+                    # does not belong to any group
+                    box.group = group_number
+                    group_number += 1
+
+                dist = 1e15
+                best_idx = -1
+                for other, box2 in enumerate(next_frame):
+                    if used[other]:
+                        continue
+                    centroid_b = box.get_centroid()
+                    centroid_b2 = box2.get_centroid()
+                    d = take_dist(*centroid_b, *centroid_b2)
+                    if best_idx == -1 or d < dist:
+                        dist = d
+                        best_idx = other
+
+                if best_idx != -1 and dist <= THRESH:
+                    # union
+                    next_frame[best_idx].group = group_number
+                    used[best_idx] = True
+
+        self.total_groupcount = group_number
+
+    def personalized_data(self):
+        FPS = 15
+        curr_time = 0
+        group_timestamps: typing.List[typing.List[float]] = [[-1, -1] for _ in range(self.total_groupcount)]
+        curr_groups = set([])
+
+        for frame in self.annotation_data:
+            seen_now = set([])
+            for box in frame:
+                group = box.group
+                seen_now.add(group)
+                if group in curr_groups:
+                    continue
+                curr_groups.add(group)
+                group_timestamps[group][0] = round(curr_time)
+
+            for group in curr_groups:
+                if group not in seen_now:
+                    group_timestamps[group][1] = round(curr_time)
+
+            curr_time += 1 / FPS
+
+        return group_timestamps
+
+    def get_mask_stats(self):
+        group_stat = [[0, 0] for _ in range(self.total_groupcount)]
+
+        for frame in self.annotation_data:
+            for box in frame:
+                group_stat[box.group][box.label] += 1
+
+        return group_stat
